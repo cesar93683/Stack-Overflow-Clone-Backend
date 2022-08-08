eec6ae72099e92e982b8459a7351c3f2681f25b0
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-08 16:15:29 -0700

    Hello World using thymeleaf. Remove devtools dependency.

--- src/main/java/com/example/redditclone/rest/RedditCloneController.java
+++ src/main/java/com/example/redditclone/rest/RedditCloneController.java
@@ -1,14 +1,14 @@
 package com.example.redditclone.rest;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RedditCloneController {
 
     @GetMapping("/")
     public String sayHello() {
-        return "Hello World";
+        return "index";
     }
 
 }
