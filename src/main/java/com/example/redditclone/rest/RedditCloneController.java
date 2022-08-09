4ee26050c8912766da8f54251f609031538b5285
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-08 17:30:18 -0700

    Convert to rest

--- src/main/java/com/example/redditclone/rest/RedditCloneController.java
+++ src/main/java/com/example/redditclone/rest/RedditCloneController.java
@@ -3,31 +3,26 @@ package com.example.redditclone.rest;
 import com.example.redditclone.entity.Post;
 import com.example.redditclone.service.PostService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class RedditCloneController {
 
     @Autowired
     private PostService postService;
 
     @GetMapping("/")
-    public String index(Model model) {
-        List<Post> posts = postService.getPosts();
-        model.addAttribute("posts", posts);
-        return "index";
+    public List<Post> getPosts() {
+        return postService.getPosts();
     }
 
     @GetMapping("/post/{id}")
-    public String getPost(@PathVariable String id, Model model) {
-        Post post = postService.getPost(id);
-        model.addAttribute("post", post);
-        return "post";
+    public Post getPost(@PathVariable String id) {
+        return postService.getPost(id);
     }
 
 }
