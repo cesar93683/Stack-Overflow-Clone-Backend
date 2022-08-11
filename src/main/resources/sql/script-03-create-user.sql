e327c5c0d6e46cae58212a72bbf0a8ba07944ed3
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-11 16:18:24 -0700

    Add sql queries for creating users

--- src/main/resources/sql/script-03-create-user.sql
+++ src/main/resources/sql/script-03-create-user.sql
@@ -1,4 +1,17 @@
+-- password is pass01
 insert into user (id, email, username, password)
-VALUES(NULL, "a@a.com", "usr123", "pass123");
+VALUES(NULL, "user01@gmail.com", "user01", "$2a$10$WRmYG3DRvHNRihopk.RCFOHeOvCe0yPZId6amT574VyEJxRRDBxvC");
+-- password is pass02
+insert into user (id, email, username, password)
+VALUES(NULL, "user02@gmail.com", "user02", "$2a$10$Ue1cDhxgg5Hm58AS4w7aHurS/zzFTQGBSfMxnKRlAMe2iaFFu9NXC");
+-- password is pass03
+insert into user (id, email, username, password)
+VALUES(NULL, "user03@gmail.com", "user03", "$2a$10$/Pp1T8qY87InP97Et4pSPuK0egPdxTBFDZjL9KSw5bdFH46ID9o3K");
+-- password is pass04
+insert into user (id, email, username, password)
+VALUES(NULL, "user04@gmail.com", "user04", "$2a$10$Yuk6YEde4zComZ2AzoliX.aLNWMYOlZnWZJKu3yZLxdRF2SgdwGZm");
+-- password is pass05
+insert into user (id, email, username, password)
+VALUES(NULL, "user05@gmail.com", "user05", "$2a$10$c8D7sSh28qon5iwSzwaYj.o47lTfdq0ECtxAKd8rFo9q1G1bsaYcy");
 
 select * from user;
