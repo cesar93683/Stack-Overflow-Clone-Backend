3489f530efe574bc5952441e62ae1c738299453e
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-12 14:50:58 -0700

    Add min and max size for LoginRequest

--- src/main/java/com/example/stackoverflowclone/payload/request/LoginRequest.java
+++ src/main/java/com/example/stackoverflowclone/payload/request/LoginRequest.java
@@ -1,11 +1,16 @@
 package com.example.stackoverflowclone.payload.request;
+
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 
 public class LoginRequest {
     @NotBlank
+    @NotBlank
+    @Size(min = 3, max = 20)
     private String username;
 
     @NotBlank
+    @Size(min = 6, max = 40)
     private String password;
 
     public String getUsername() {
