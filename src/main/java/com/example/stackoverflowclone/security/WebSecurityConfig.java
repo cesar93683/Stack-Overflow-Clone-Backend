b61c54139d546e92ac3edc2e03ebebed38629e02
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-13 14:08:08 -0700

    Unauthenticated users can now get posts

--- src/main/java/com/example/stackoverflowclone/security/WebSecurityConfig.java
+++ src/main/java/com/example/stackoverflowclone/security/WebSecurityConfig.java
@@ -53,9 +53,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         http.cors().and().csrf().disable()
                 .exceptionHandling().authenticationEntryPoint(unauthorizedHandler).and()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-                .authorizeRequests().antMatchers("/api/auth/**").permitAll()
-                .antMatchers("/api/test/**").permitAll()
-                .anyRequest().authenticated();
+                .authorizeRequests().anyRequest().permitAll();
+
         http.addFilterBefore(authenticationJwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 }
