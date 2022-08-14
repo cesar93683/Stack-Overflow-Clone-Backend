bb0f9ce153dd2a60fefa128e69833ee455e8fe4a
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-08-14 15:13:01 -0700

    Update create post database script

--- src/main/resources/sql/script-04-create-post-table.sql
+++ src/main/resources/sql/script-04-create-post-table.sql
@@ -5,8 +5,10 @@ DROP TABLE IF EXISTS `post`;
 
 CREATE TABLE `post` (
   `id` int NOT NULL AUTO_INCREMENT,
-  `user_id` int DEFAULT NULL,
-  `title` varchar(50) DEFAULT NULL,
-  `content` varchar(500) DEFAULT NULL,
+  `user_id` int NOT NULL,
+  `title` varchar(50) NOT NULL,
+  `content` varchar(500) NOT NULL,
+  `created_at` DATETIME NOT NULL,
+  `updated_at` DATETIME,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
