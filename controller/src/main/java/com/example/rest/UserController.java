4154772f176f961c7067f98c28bf4234eb24bb8f
Cesar <48188267+cesar93683@users.noreply.github.com>
2022-09-07 16:29:32 -0700

    Add CrossOrigin annotation to UserController

--- controller/src/main/java/com/example/rest/UserController.java
+++ controller/src/main/java/com/example/rest/UserController.java
@@ -6,11 +6,9 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/users")
 public class UserController {
