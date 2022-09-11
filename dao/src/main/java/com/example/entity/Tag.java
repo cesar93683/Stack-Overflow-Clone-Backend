f05bc2ca0db1ac919d9123544ef6c6037f5b9c56
cesar93683 <48188267+cesar93683@users.noreply.github.com>
2022-09-11 11:45:54 -0700

    Add Tag entity

---
+++ dao/src/main/java/com/example/entity/Tag.java
@@ -0,0 +1,17 @@
+package com.example.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "tag")
+public class Tag {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @Column(name = "tag", nullable = false, updatable = false)
+    private String tag;
+
+}
